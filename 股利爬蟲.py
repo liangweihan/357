48b7108380e8d5c1a966e476e54b92d94e3fa8f5
liangweihan <144086846+liangweihan@users.noreply.github.com>
2023-11-09 00:45:05 +0800

    Update 股利爬蟲.py

--- 股利爬蟲.py
+++ 股利爬蟲.py
@@ -6,17 +6,18 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)  # 初始化CORS
 
-@app.route('/get_stock_data', methods=['POST'])
-@cross_origin()  # 允许所有域的跨域请求
-def get_stock_data():
-    stock_code = request.json.get('stockCode')
-
+def scrape_stock_data(stock_code):
     url = f'https://tw.stock.yahoo.com/quote/{stock_code}.TW/dividend'
     response = requests.get(url)
 
-    if response.status_code == 200:
+    try:
+        # 确保请求成功
+        response.raise_for_status()
+        
+        # 使用 BeautifulSoup 解析页面内容
         soup = BeautifulSoup(response.content, 'html.parser')
 
+        # 提取股票数据
         times = soup.find_all("div", class_="D(f) Start(0) H(100%) Ai(c) Bgc(#fff) table-row:h_Bgc(#e7f3ff) Pstart(12px) Pend(12px) Bdrststart(4px) Bdrsbstart(4px) Pos(r) Bxz(bb) Z(2)")
         dividend_periods = [time.text for time in times[:5]] if times else []
 
@@ -28,12 +29,18 @@ def get_stock_data():
             'cash_dividends': cash_dividends
         }
 
-        return jsonify({'success': True, 'data': data})
+        return {'success': True, 'data': data}
 
-    else:
-        return jsonify({'success': False, 'message': '請求失敗'})
+    except requests.RequestException as e:
+        # 请求失败处理
+        return {'success': False, 'message': f'請求失敗: {e}'}
+
+@app.route('/get_stock_data', methods=['POST'])
+@cross_origin()  # 允许所有域的跨域请求
+def get_stock_data():
+    stock_code = request.json.get('stockCode')
+    stock_data = scrape_stock_data(stock_code)
+    return jsonify(stock_data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=5000)
-
-
