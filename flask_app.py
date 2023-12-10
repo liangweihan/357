10122e7197992d40d49ef511bd8906a1e7d28d28
liangweihan <144086846+liangweihan@users.noreply.github.com>
2023-12-11 02:09:09 +0800

    357

--- flask_app.py
+++ flask_app.py
@@ -15,19 +15,23 @@ def scrape_stock_data(stock_code):
         soup = BeautifulSoup(response.content, 'html.parser')
 
         times = soup.find_all("div", class_="D(f) Start(0) H(100%) Ai(c) Bgc(#fff) table-row:h_Bgc(#e7f3ff) Pstart(12px) Pend(12px) Bdrststart(4px) Bdrsbstart(4px) Pos(r) Bxz(bb) Z(2)")
-        dividend_periods = [time.text for time in times[:5]] if times else []
+        dividend_periods = [time.text for time in times] if times else []
 
         moneys = soup.find_all("div", class_="Fxg(1) Fxs(1) Fxb(0%) Ta(end) Mend($m-table-cell-space) Mend(0):lc Miw(62px)")
-        cash_dividends = [float(money.text) for index, money in enumerate(moneys[2:], start=1) if index % 2 != 0][:5] if moneys else []
+        all_cash_dividends = [float(money.text) for index, money in enumerate(moneys[2:], start=1) if index % 2 != 0] if moneys else []
 
-        # 新增計算便宜價、合理價和昂貴價
-        cheap_price = round((sum(cash_dividends) * 20) / 7, 2)
-        fair_price = round((sum(cash_dividends) * 20) / 5, 2)
-        expensive_price = round((sum(cash_dividends) * 20) / 3, 2)
+        # 只保留前五笔现金股利并计算其总和
+        cash_dividends = all_cash_dividends[:5]
+        total_cash_dividends = sum(cash_dividends)
+
+        # 新增计算便宜价、合理价和昂贵价
+        cheap_price = round((total_cash_dividends * 20) / 7, 2)
+        fair_price = round((total_cash_dividends * 20) / 5, 2)
+        expensive_price = round((total_cash_dividends * 20) / 3, 2)
 
         data = {
-            'dividend_periods': dividend_periods,
-            'cash_dividends': cash_dividends,
+            'dividend_periods': dividend_periods,  # 返回所有的股利所屬期間資料
+            'cash_dividends': all_cash_dividends,  # 返回所有的股利金額
             'cheap_price': cheap_price,
             'fair_price': fair_price,
             'expensive_price': expensive_price
@@ -38,6 +42,7 @@ def scrape_stock_data(stock_code):
     except requests.RequestException as e:
         return {'success': False, 'message': f'Request failed: {e}'}
 
+
 @app.route('/get_stock_data', methods=['POST'])
 def get_stock_data():
     stock_code = request.json.get('stockCode')
@@ -46,3 +51,4 @@ def get_stock_data():
 
 if __name__ == '__main__':
     app.run(debug=True)  # 運行在本地的 127.0.0.1:5000
+
