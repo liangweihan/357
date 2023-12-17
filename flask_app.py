d9811933265e05ef6efd61da329821c6a95b927b
liangweihan <144086846+liangweihan@users.noreply.github.com>
2023-12-17 17:46:58 +0800

    2

--- flask_app.py
+++ flask_app.py
@@ -1,54 +1,103 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import requests
-from bs4 import BeautifulSoup
+from flask import Flask, request, jsonify  # 引入 Flask 相關模組
+from flask_cors import CORS  # 引入處理跨域請求的模組
+import requests  # 用於發送 HTTP 請求
+from bs4 import BeautifulSoup  # 用於解析 HTML 內容
 
-app = Flask(__name__)
-CORS(app)
+app = Flask(__name__)  # 初始化 Flask 應用程式
+CORS(app)  # 處理跨域請求
 
 def scrape_stock_data(stock_code):
-    url = f'https://tw.stock.yahoo.com/quote/{stock_code}.TW/dividend'
-    response = requests.get(url)
+    """
+    爬取股票資料的函數
+    
+    Args:
+        stock_code (str): 股票代碼
+    
+    Returns:
+        dict: 包含股票資料的字典格式
+    """
+    def scrape_dividend_data():
+        url = f'https://tw.stock.yahoo.com/quote/{stock_code}.TW/dividend'  # Yahoo股票網頁的URL
+        response = requests.get(url)  # 發送HTTP GET請求取得網頁內容
 
-    try:
-        response.raise_for_status()
-        soup = BeautifulSoup(response.content, 'html.parser')
+        try:
+            response.raise_for_status()  # 檢查HTTP請求狀態碼是否為200，否則拋出異常
+            soup = BeautifulSoup(response.content, 'html.parser')  # 使用BeautifulSoup解析HTML內容
 
-        times = soup.find_all("div", class_="D(f) Start(0) H(100%) Ai(c) Bgc(#fff) table-row:h_Bgc(#e7f3ff) Pstart(12px) Pend(12px) Bdrststart(4px) Bdrsbstart(4px) Pos(r) Bxz(bb) Z(2)")
-        dividend_periods = [time.text for time in times] if times else []
+            # 獲取股利所屬期間的資料
+            times = soup.find_all("div", class_="D(f) W(88px) Ta(start)")
+            dividend_periods = [time.text.strip() for time in times[2:]] if times else []  # 只保留第三筆以後的股利所屬期間資料
 
-        moneys = soup.find_all("div", class_="Fxg(1) Fxs(1) Fxb(0%) Ta(end) Mend($m-table-cell-space) Mend(0):lc Miw(62px)")
-        all_cash_dividends = [float(money.text) for index, money in enumerate(moneys[2:], start=1) if index % 2 != 0] if moneys else []
+            # 獲取所有股利金額的資料
+            moneys = soup.find_all("div", class_="Fxg(1) Fxs(1) Fxb(0%) Ta(end) Mend(0):lc Mend(12px) W(72px) Miw(72px)")
+            all_cash_dividends = [money.text.strip() for money in moneys[4:]] if moneys else []  # 只保留第五筆以後的股利金額資料
 
-        # 只保留前五笔现金股利并计算其总和
-        cash_dividends = all_cash_dividends[:5]
-        total_cash_dividends = sum(cash_dividends)
+            # 對每四個現金股利資料進行處理
+            processed_cash_dividends = []
+            for i in range(0, len(all_cash_dividends), 4):
+                chunk = all_cash_dividends[i:i+4]  # 每四個資料為一循環
 
-        # 新增计算便宜价、合理价和昂贵价
-        cheap_price = round((total_cash_dividends * 20) / 7, 2)
-        fair_price = round((total_cash_dividends * 20) / 5, 2)
-        expensive_price = round((total_cash_dividends * 20) / 3, 2)
+                # 前兩個資料若有不是數值的以0取代，後面兩個資料也以0取代，然後將四個資料加總
+                processed_chunk = [float(dividend) if dividend.replace('.', '', 1).isdigit() else 0 for dividend in chunk[:2]]
+                total_chunk = sum(processed_chunk)
+                processed_cash_dividends.append(total_chunk)
 
-        data = {
-            'dividend_periods': dividend_periods,  # 返回所有的股利所屬期間資料
-            'cash_dividends': all_cash_dividends,  # 返回所有的股利金額
-            'cheap_price': cheap_price,
-            'fair_price': fair_price,
-            'expensive_price': expensive_price
+            total_cash_dividends = sum(processed_cash_dividends[:5])
+
+            # 計算便宜價、合理價和昂貴價
+            cheap_price = round((total_cash_dividends * 20) / 7, 2)
+            fair_price = round((total_cash_dividends * 20) / 5, 2)
+            expensive_price = round((total_cash_dividends * 20) / 3, 2)
+
+            data = {
+                'dividend_periods_357': dividend_periods[:5],
+                'cash_dividends_357': processed_cash_dividends[:5],
+                'dividend_periods_di': dividend_periods[:10],
+                'cash_dividends_di': processed_cash_dividends[:10],
+                'cheap_price': cheap_price,
+                'fair_price': fair_price,
+                'expensive_price': expensive_price
+            }
+
+            return {'success': True, 'data': data}
+
+        except requests.RequestException as e:
+            return {'success': False, 'message': f'Request failed: {e}'}
+        
+    def scrape_revenue_data():
+        url = f'https://tw.stock.yahoo.com/quote/{stock_code}.TW/revenue'
+        response = requests.get(url)
+
+        try:
+            response.raise_for_status()
+            soup = BeautifulSoup(response.content, 'html.parser')
+            
+            # 从HTML中提取日期数据的示例
+            date_divs = soup.find_all("div", class_="W(65px) Ta(start)")
+            dates = [date.text.strip() for date in date_divs]
+
+            return {'success': True, 'data': dates}  # 修改此处返回所需数据
+
+        except requests.RequestException as e:
+            return {'success': False, 'message': f'Request failed: {e}'}
+
+    dividend_data = scrape_dividend_data()
+    revenue_data = scrape_revenue_data()
+
+    if dividend_data['success'] and revenue_data['success']:
+        combined_data = {
+            'dividend_data': dividend_data['data'],
+            'revenue_data': revenue_data['data']
         }
-
-        return {'success': True, 'data': data}
-
-    except requests.RequestException as e:
-        return {'success': False, 'message': f'Request failed: {e}'}
-
+        return {'success': True, 'data': combined_data}
+    else:
+        return {'success': False, 'message': 'Failed to fetch data'}
 
 @app.route('/get_stock_data', methods=['POST'])
 def get_stock_data():
-    stock_code = request.json.get('stockCode')
-    stock_data = scrape_stock_data(stock_code)
-    return jsonify(stock_data)
+    stock_code = request.json.get('stockCode')  # 从POST请求中获取股票代码
+    stock_data = scrape_stock_data(stock_code)  # 调用函数爬取股票数据
+    return jsonify(stock_data)  # 将股票数据以JSON格式返回
 
 if __name__ == '__main__':
-    app.run(debug=True)  # 運行在本地的 127.0.0.1:5000
-
+    app.run(debug=True)  # 运行在本地的127.0.0.1:5000，启用Debug模式
